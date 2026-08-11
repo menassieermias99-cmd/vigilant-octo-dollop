c21fcf9a98f77a84290b57dd6dce0f5e2f5e03fa
menassieermias99-cmd <menassieermias99@gmail.com>
2026-08-11 09:53:34 +0300

    added: authentication to build a page

--- src/app/builder/[id]/page.tsx
+++ src/app/builder/[id]/page.tsx
@@ -1,12 +1,16 @@
-import { getFormAction } from "@/app/actions";
+import { getFormAction, verifyAuth } from "@/app/actions";
 import FormBuilder from "@/components/FormBuilder";
-import { notFound } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 
 interface BuidlerPageProps {
   params: Promise<{ id: string }>;
 }
 
 export default async function BuilderPage({ params }: BuidlerPageProps) {
+  4;
+  const isAuthenticated = await verifyAuth();
+  if (!isAuthenticated) redirect("/login");
+
   const { id } = await params;
   const form = await getFormAction(id);
 
